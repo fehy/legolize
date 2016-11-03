87e46986abcb0d8e76e4a972b3eda1ca1a507760
Miroslav Chomut <chmirko@gmail.com>
2016-11-03 23:21:17 +0100

    WhiteBackground

--- GUI/OpenGL.cpp
+++ GUI/OpenGL.cpp
@@ -52,7 +52,7 @@ GLvoid OpenGL::resizeGLScene(GLsizei width, GLsizei height)
 int OpenGL::initGL()
 {
 	glShadeModel(GL_SMOOTH);							// Enable Smooth Shading
-	glClearColor(0.0f, 0.0f, 0.0f, 0.5f);			// Black Background
+	glClearColor(0.9f, 0.9f, 0.9f, 0.5f);			// Black Background
 	glClearDepth(1.0f);									// Depth Buffer Setup
 	glEnable(GL_DEPTH_TEST);							// Enables Depth Testing
 	glDepthFunc(GL_LEQUAL);								// The Type Of Depth Testing To Do
