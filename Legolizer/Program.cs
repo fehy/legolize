43770c672a09c20493ba871364df6ee7c77bd119
honza <honza@DESKTOP-LB4BMVP>
2016-11-04 02:19:07 +0100

    barclays logo

--- Legolizer/Program.cs
+++ Legolizer/Program.cs
@@ -10,7 +10,12 @@ namespace Legolizer
             //var bricks = Doer.Do(PointCloudGen.Generator.Cube(10, 8, 2));            
             //var bricks = Doer.Do(PointCloudGen.Generator.Clock(15, 4));
             //var bricks = Doer.Do(PointCloudGen.Generator.Cone(4, 15, 1));
-            var bricks = Doer.Do(PointCloudGen.Generator.RotatedClock(15, 4));
+
+            //var bricks = Doer.Do(PointCloudGen.Generator.GenerateFromBMP(@"..\..\..\PointCloudGen\absaImage.bmp", 100, 200, 5));
+            //var bricks = Doer.Do(PointCloudGen.Generator.GenerateFromBMP(@"..\..\..\PointCloudGen\barclays.bmp", 50, 250, 1));
+            var bricks = Doer.Do(PointCloudGen.Generator.GenerateFromBMP(@"..\..\..\PointCloudGen\barclaysSmall.bmp", 41, 250, 5));
+
+            //var bricks = Doer.Do(PointCloudGen.Generator.RotatedClock(15, 4));
 
             /*var bricks = new LegoModeler.Brick[4];
             bricks[0] = new LegoModeler.Brick(BrickType.B4x2, 2, 1, 0, BrickRotation.R0);
@@ -20,7 +25,7 @@ namespace Legolizer
 
             //new GenerateStaticBricks().OutputToLdr(bricks);
 
-            new OpenGLSceneWriter("..\\..\\..\\Scenes\\Scene.scene").Write(bricks);
+            //new OpenGLSceneWriter("..\\..\\..\\Scenes\\Scene.scene").Write(bricks);
         }
     }
 }
